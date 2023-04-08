69a2a20453bf757dbe38a37bfaa052760bca37a1
flx777 <subtirica.florin29@gmail.com>
2023-04-08 21:25:37 +0300

    To implement ARP and Binary Search

--- router.c
+++ router.c
@@ -6,29 +6,43 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define ICMP_ECHO_REPLY 0
-#define ICMP_ECHO_REQUEST 8
-#define ICMP_DEST_UNREACHABLE 3
-#define ICMP_TIME_EXCEEDED 11
+#define ICMP_ECHO_REPLY 		0
+#define ICMP_DEST_UNREACHABLE 	3
+#define ICMP_ECHO_REQUEST 		8
+#define ICMP_TIME_EXCEEDED 		11
 
-#define DROP_PACKAGE -1
-#define GOOD_PACKAGE  0
+#define ARP_OP_REQUEST 			1
+#define ARP_OP_REPLY 			2
+
+#define DROP_PACKAGE 			-1
+#define GOOD_PACKAGE  			0
 
 /* Routing table */
 struct route_table_entry *rtable;
-int rtable_len;
+size_t rtable_len;
 
 /* ARP table */
 struct arp_entry *arp_table;
-int arp_table_len;
+size_t arp_table_len;
+
+struct ether_header *eth_hdr;
+struct iphdr *ip_hdr;
+struct icmphdr *icmp_hdr;
+
+struct arp_entry *mac;
 
 struct route_table_entry *get_best_route(uint32_t ip_dest) {
 	struct route_table_entry *best_route = NULL;
 
 	for (size_t i = 0; i < rtable_len; i++) {
-		if ((rtable[i].mask & ip_dest) == (rtable[i].prefix & rtable[i].mask)) {
-			if (best_route == NULL) best_route = &rtable[i];
-			else if (best_route->mask < rtable[i].mask) best_route = &rtable[i];
+		if ((rtable[i].mask & ip_dest)
+			 ==
+			(rtable[i].prefix & rtable[i].mask)) {
+
+			if (!best_route)
+				best_route = &rtable[i];
+			else if (best_route->mask < rtable[i].mask)
+				best_route = &rtable[i];
 		}
 	}
 
@@ -46,58 +60,57 @@ struct arp_entry *get_arp_entry(uint32_t ip_dest) {
 }
 
 void icmp(int interface, int type,
-		  struct ether_header *eth_hdr,
-		  char buf[MAX_PACKET_LEN],
-		  size_t len,
-		  struct iphdr *ip_hdr);
+		  char buf[MAX_PACKET_LEN]);
 
 int ipv4(int interface,
-		  struct ether_header *eth_hdr,
 		  char buf[MAX_PACKET_LEN],
-		  size_t len,
-		  struct iphdr *ip_hdr) {
+		  size_t len) {
+	if (inet_addr(get_interface_ip(interface)) == ip_hdr->daddr
+		&& icmp_hdr->type == ICMP_ECHO_REQUEST) {
+		icmp(interface, ICMP_ECHO_REPLY, buf);
+
+		return GOOD_PACKAGE;
+	}
 
 	// Save the checksum
 	uint16_t dummy = ip_hdr->check;
 	ip_hdr->check = 0;	
 
 	// If checksum is wrong, throw the packet
-	if (checksum((uint16_t *) ip_hdr, sizeof(struct iphdr)) != ntohs(dummy)) return DROP_PACKAGE;
-
-	printf("Checksum is good\n");
+	if (checksum((void *) ip_hdr, sizeof(struct iphdr)) != ntohs(dummy)) return DROP_PACKAGE;
 
 	//Find best matching route
 	struct route_table_entry *route = get_best_route(ip_hdr->daddr);
 
 	// If route is NULL, send ICMP destination unreachable
 	if (!route) {
-		icmp(interface, ICMP_DEST_UNREACHABLE, eth_hdr, buf, len, ip_hdr);
+		icmp(interface, ICMP_DEST_UNREACHABLE, buf);
 
 		return DROP_PACKAGE;
 	}
 
 	// If TTL is less or equal to 1, send ICMP time exceeded
-	if (ntohs(ip_hdr->ttl) <= 1) {
-		icmp(interface, ICMP_TIME_EXCEEDED, eth_hdr, buf, len, ip_hdr);
+	if (ip_hdr->ttl <= 1) {
+		icmp(interface, ICMP_TIME_EXCEEDED, buf);
 
 		return DROP_PACKAGE;
 	}
 
 	//Update TTL and checksum
 	ip_hdr->ttl--;
-	ip_hdr->check = htons(checksum((uint16_t *) ip_hdr, sizeof(struct iphdr)));
+	ip_hdr->check = htons(checksum((void *) ip_hdr, sizeof(struct iphdr)));
 
-	struct arp_entry *arp = get_arp_entry(route->next_hop);
+	mac = get_arp_entry(route->next_hop);
 
 	// If ARP entry is NULL, send ICMP destination unreachable
-	if (!arp) {
-		icmp(interface, ICMP_DEST_UNREACHABLE, eth_hdr, buf, len, ip_hdr);
+	if (!mac) {
+		icmp(interface, ICMP_DEST_UNREACHABLE, buf);
 
 		return DROP_PACKAGE;
 	}
 
 	//Update the destination MAC address
-    memcpy(eth_hdr->ether_dhost, arp->mac, sizeof(arp->mac));
+    memcpy(eth_hdr->ether_dhost, mac->mac, sizeof(mac->mac));
 
 	//Update the source MAC address, by getting the address of the 
 	//best route interface;
@@ -109,17 +122,43 @@ int ipv4(int interface,
 	return GOOD_PACKAGE;
 }
 
-int arp(int interface, struct ether_header *eth_hdr) {
+int arp(int interface) {
+
 	return DROP_PACKAGE;
 }
 
 void icmp(int interface, int type,
-		  struct ether_header *eth_hdr,
-		  char buf[MAX_PACKET_LEN],
-		  size_t len,
-		  struct iphdr *ip_hdr) {
+		  char buf[MAX_PACKET_LEN]) {
+	size_t len = sizeof(struct ether_header) + sizeof(struct iphdr) + sizeof(struct icmphdr);
 
-	
+	u_int8_t temp[6];
+	memcpy(temp, eth_hdr->ether_dhost, sizeof(eth_hdr->ether_dhost));
+	memcpy(eth_hdr->ether_dhost, eth_hdr->ether_shost, sizeof(eth_hdr->ether_shost));
+	memcpy(eth_hdr->ether_shost, temp, sizeof(temp));
+	eth_hdr->ether_type = htons(0x0800);
+
+	uint32_t aux = ip_hdr->saddr; 
+	ip_hdr->saddr = ip_hdr->daddr;
+	ip_hdr->daddr = aux;
+	ip_hdr->version = 4;
+	ip_hdr->ihl = 5;
+	ip_hdr->tos = 0;
+	ip_hdr->tot_len = htons(sizeof(struct iphdr) + sizeof(struct icmphdr));
+	ip_hdr->id = htons(1);
+	ip_hdr->frag_off = 0;
+	ip_hdr->ttl = 64;
+	ip_hdr->protocol = IPPROTO_ICMP;
+	ip_hdr->check = 0;
+	ip_hdr->check = htons(checksum((void *) ip_hdr, sizeof(struct iphdr)));
+
+	icmp_hdr->code = 0;
+	icmp_hdr->type = type;
+	icmp_hdr->checksum = 0;
+	icmp_hdr->checksum = htons(checksum((void *) icmp_hdr, sizeof(struct icmphdr)));
+
+    memcpy(eth_hdr->ether_dhost, mac->mac, sizeof(mac->mac));
+
+	send_to_link(interface, buf, len);
 }
 
 int main(int argc, char *argv[])
@@ -129,36 +168,37 @@ int main(int argc, char *argv[])
 	// Do not modify this line
 	init(argc - 2, argv + 2);
 
-	/* Code to allocate the MAC and route tables */
-	rtable 		  = malloc(sizeof(struct route_table_entry) * 100000);
+	/* Code to allocate the MAC/ARP and route tables */
+	rtable 		  	= malloc(sizeof(struct route_table_entry) * 80000);
 	/* DIE is a macro for sanity checks */
-	DIE(rtable == NULL, "memory");
+	DIE(!rtable, 	"Failed to allocate memory for routing table.");
 
-	arp_table 	  = malloc(sizeof(struct arp_entry) * 100);
-	DIE(arp_table == NULL, "memory");
+	arp_table 	  	= malloc(sizeof(struct arp_entry) * 100);
+	DIE(!arp_table, "Failed to allocate memory for routing table.");
 	
 	/* Read the static routing table and the MAC table */
-	rtable_len 	  = read_rtable(argv[1], rtable);
-	arp_table_len = parse_arp_table("arp_table.txt", arp_table);
+	rtable_len 	  	= read_rtable(argv[1], rtable);
+	arp_table_len 	= parse_arp_table("arp_table.txt", arp_table);
 
 receive_loop:
 
 	int interface;
 	size_t len;
 
-	interface = recv_from_any_link(buf, &len);
+	interface 		= recv_from_any_link(buf, &len);
 	DIE(interface < 0, "recv_from_any_links");
-	printf("We have received a packet\n");
 
-	struct ether_header *eth_hdr = (struct ether_header *) buf;
-	struct iphdr *ip_hdr = (struct iphdr *) (buf + sizeof(struct ether_header));
+	eth_hdr 		= (struct ether_header *) buf;
+	ip_hdr 			= (struct iphdr *) (buf + sizeof(struct ether_header));
+	icmp_hdr 		= (struct icmphdr *) (buf + sizeof(struct ether_header)
+											  + sizeof(struct iphdr));
 
 	switch (ntohs(eth_hdr->ether_type)) {
 	case 0x0800:
-		if (ipv4(interface, eth_hdr, buf, len, ip_hdr) == DROP_PACKAGE) goto receive_loop;
+		if (ipv4(interface, buf, len) == DROP_PACKAGE) goto receive_loop;
 		break;
 	case 0x0806:
-		if (arp(interface, eth_hdr) == DROP_PACKAGE) goto receive_loop;
+		if (arp(interface) == DROP_PACKAGE) goto receive_loop;
 		break;
 	default:
 		goto receive_loop;
